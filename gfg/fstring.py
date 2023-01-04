94c269a155b25d3c90d4e56293fd21df8cb214ba
Jishnu <jishnusurajila@gmail.com>
2023-01-04 20:19:50 +0530

    [gfg] : fstring program added (#3)
    * dsa: renamed note to REAME.md
    
    * prg: python simple fstring program added

---
+++ gfg/fstring.py
@@ -0,0 +1,10 @@
+# program to explore f string in python
+
+if __name__ == "__main__":
+    first_name = "Sherlock"
+    last_name = "Holmes"
+    address = "221 B, Baker Street"
+
+    print(
+        f"My name is {first_name} {last_name} and the address {address} ...tun tun tundun..tundun (music!)"
+    )
