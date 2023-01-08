ab6971de0fcecb687bde2ccc4c70f8b468188a36
lonebots <jishnusurajila@gmail.com>
2023-01-08 13:04:15 +0530

    fix: updated merge sorted linkedlist

--- leet-code/linkedlist/easy/1-merge_sorted_linkedlist.py
+++ leet-code/linkedlist/easy/1-merge_sorted_linkedlist.py
@@ -1,25 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-class Solution():
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
         curr = head = ListNode()
-        while list1 and list2 :
-            if list1.val < list2.val :
+        while list1 and list2:
+            if list1.val < list2.val:
                 curr.next = list1
                 list1 = list1.next
                 curr = curr.next
-            else :
+            else:
                 curr.next = list2
                 list2 = list2.next
                 curr = curr.next
-        if list1 or list2 :
+        if list1 or list2:
             curr.next = list1 if list1 else list2
-        
+
         return head.next
 
 
-
+if __name__ == "__main__":
+    pass
