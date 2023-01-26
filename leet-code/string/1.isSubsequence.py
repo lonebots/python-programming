656d78b92740bf909acfec8f19e726890a76406c
lonebots <jishnusurajila@gmail.com>
2023-01-26 23:08:30 +0530

    lc:is sequence string solution added

---
+++ leet-code/string/1.isSubsequence.py
@@ -0,0 +1,18 @@
+'''
+question : https://leetcode.com/problems/is-subsequence/description/?envType=study-plan&id=level-1
+'''
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0 
+        j = 0 
+        while (i < len(s) and j < len(t)) :
+            if s[i] == t[j] :
+                i += 1
+                j += 1 
+            else :
+                j += 1
+        if i == len(s) :
+            return True
+        else :
+            return False
