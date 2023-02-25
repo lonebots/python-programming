96728e9fafaae381411e9cf7594dfcf68bbdb08f
lonebots <jishnusurajila@gmail.com>
2023-02-25 16:56:13 +0530

    leetcode: unique path solution added

---
+++ leet-code/62-unique-paths.py
@@ -0,0 +1,14 @@
+import math
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # mathematical
+        #return int(math.factorial(m+n-2)/(math.factorial(n-1) * math.factorial(m-1)))
+        
+        # dynamic programming solution 
+        row = [1] * n 
+        for i in range(m-1) :
+            newRow = [1] * n 
+            for j in range(n-2,-1,-1) :
+                newRow[j] = newRow[j+1] + row[j]
+            row = newRow
+        return row[0]
