ad09d7c24e0d0ff8541c5b5018e12331b6f5ca21
lonebots <jishnusurajila@gmail.com>
2023-02-28 18:49:35 +0530

    lc75:2. best time to buy and sell stocks I solution added

---
+++ leet-code/leetcode75/2-121-best-time-to-bus-and-sell-stocks-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        # approach 1 : brute force (TLE)
+        # max_profit = 0 
+        # for i in range(len(prices)) :
+        #     for j in range(i+1,len(prices)) :
+        #         if prices[j] - prices[i] > max_profit :
+        #             max_profit = prices[j] - prices[i]
+
+        # return max_profit
+
+        # # approach 2 - One pass
+        max_profit = 0
+        min_price = float('inf')
+
+        for i in range(len(prices)) :
+            if prices[i] < min_price :
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit :
+                max_profit = prices[i] - min_price
+
+        return max_profit  
