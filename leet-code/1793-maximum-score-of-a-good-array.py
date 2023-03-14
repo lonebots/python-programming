2b366e037a6c2f63fda318d4f7051031880ff9e9
lonebots <jishnusurajila@gmail.com>
2023-03-14 10:48:16 +0530

    lc : 1793. maximum score of a good array solution added

---
+++ leet-code/1793-maximum-score-of-a-good-array.py
@@ -0,0 +1,53 @@
+'''
+You are given an array of integers nums (0-indexed) and an integer k.
+
+The score of a subarray (i, j) is defined as min(nums[i], nums[i+1], ..., nums[j]) * (j - i + 1). A good subarray is a subarray where i <= k <= j.
+
+Return the maximum possible score of a good subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,3,7,4,5], k = 3
+Output: 15
+Explanation: The optimal subarray is (1, 5) with a score of min(4,3,7,4,5) * (5-1+1) = 3 * 5 = 15. 
+
+Example 2:
+
+Input: nums = [5,5,4,5,4,1,1,1], k = 0
+Output: 20
+Explanation: The optimal subarray is (0, 4) with a score of min(5,5,4,5,4) * (4-0+1) = 4 * 5 = 20.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    1 <= nums[i] <= 2 * 104
+    0 <= k < nums.length
+
+
+'''
+
+'''
+Approach : Two pointer method 
+    * Initialize left and right pointer to the k and from there travel to extreme ends of the nums array.
+    * update left and right based on the condition
+'''
+
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        left = right = k 
+        max_score = min_score = nums[k]
+
+        while left > 0 or right < len(nums) - 1 :
+            if (nums[left-1] if left else 0) < (nums[right+1] if right < len(nums) -1  else 0 ) :
+                right += 1
+            else :
+                left -= 1 
+
+            min_score = min(min_score,nums[left],nums[right])
+            max_score = max(max_score, min_score * (right - left + 1))
+
+        return max_score
