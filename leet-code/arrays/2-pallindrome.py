54c2b6af3969067b67f1db12df85b6cdf5fbc1d5
lonebots <jishnusurajila@gmail.com>
2023-03-18 23:24:35 +0530

    lc : array palindrome check additional method added

--- leet-code/arrays/2-pallindrome.py
+++ leet-code/arrays/2-pallindrome.py
@@ -1,12 +1,19 @@
 class solution :
 
-    def is_palindrome(self, x) -> bool :
-        temp = x 
-        reverse = 0
-        while x > 0 :
-            reverse = ((reverse * 10) + (x % 10))
-            x = x // 10
-        return temp == reverse 
+# method 1 : loop and reverse
+    # def is_palindrome(self, x) -> bool :
+    #     temp = x 
+    #     reverse = 0
+    #     while x > 0 :
+    #         reverse = ((reverse * 10) + (x % 10))
+    #         x = x // 10
+    #     return temp == reverse 
+    
+
+# method 2 : using python inbuild string method 
+    def is_palindrome(self,x) -> bool : 
+        return int(str(x)[::-1]) == x 
+
 
 if __name__ == "__main__" :
     number = int(input(">"))
