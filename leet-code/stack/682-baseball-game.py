83bcca242e32ee71f6305beebac8959315f06b29
lonebots <jishnusurajila@gmail.com>
2023-03-23 13:39:15 +0530

    lc:stack: 682. baseball game added

---
+++ leet-code/stack/682-baseball-game.py
@@ -0,0 +1,36 @@
+'''
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+    An integer x.
+        Record a new score of x.
+    '+'.
+        Record a new score that is the sum of the previous two scores.
+    'D'.
+        Record a new score that is the double of the previous score.
+    'C'.
+        Invalidate the previous score, removing it from the record.
+
+Return the sum of all the scores on the record after applying all the operations.
+
+The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+'''
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+
+        for op in operations :
+            if op == 'C' :
+                stack.pop()
+            elif op == 'D' :
+                stack.append(2*stack[-1])
+            
+            elif op == '+' :
+                stack.append(stack[-1]+ stack[-2])
+            else :
+                stack.append(int(op))
+            print(stack)
+
+        return sum(stack)
