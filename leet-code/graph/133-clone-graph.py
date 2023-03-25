9b48b34ff8685dadda632a527d2e3c6dd977a140
lonebots <jishnusurajila@gmail.com>
2023-03-25 11:25:35 +0530

    lc:graph: 133. clone graph solution added

---
+++ leet-code/graph/133-clone-graph.py
@@ -0,0 +1,48 @@
+'''
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+'''
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldToNew = {}
+
+        def dfs(node) :
+            if node in oldToNew :
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+
+            for neighbor in node.neighbors :
+                copy.neighbors.append(dfs(neighbor))
+
+            return copy
+
+        return dfs(node) if node else None
