9da9eaf9f13dae9e6e8832db104d5f9dc51e9357
lonebots <jishnusurajila@gmail.com>
2023-05-22 11:03:26 +0530

    lc: 347. top k frequet elements solution added

---
+++ leet-code/347-top-k-frequent-elements.py
@@ -0,0 +1,40 @@
+'''
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    k is in the range [1, the number of unique elements in the array].
+    It is guaranteed that the answer is unique.
+'''
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        freq_count = {} 
+        for n in nums :
+            freq_count[n] = freq_count.get(n,0) + 1 
+
+        # sort the dictionary
+        answer = list(k for k,v in sorted(freq_count.items(),key = lambda item : item[1]))[::-1]
+        return answer[:k]
