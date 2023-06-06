db99b847fd067a8d8a75b9418ddb1d98884d6ff8
lonebots <jishnusurajila@gmail.com>
2023-06-06 08:48:41 +0530

    lc: 1502. can make arithmetic progression from sequence solution added

---
+++ leet-code/1502-can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,26 @@
+''' 
+A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+
+Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: arr = [3,5,1]
+Output: true
+Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
+'''
+
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        arr.sort()
+
+        for i in range(len(arr)-2) :
+            if arr[i+1] - arr[i] != arr[i+2] - arr[i+1] :
+                return False 
+        return True
