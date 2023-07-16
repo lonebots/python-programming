d0136db83dda19566ec57a09c5eac35ffbb7a9c5
lonebots <jishnusurajila@gmail.com>
2023-07-16 16:28:00 +0530

    lc: 1125. smallest sufficien team solution added

---
+++ leet-code/1125-smallest-sufficient-team.py
@@ -0,0 +1,71 @@
+'''
+In a project, you have a list of required skills req_skills, and a list of people. The ith person people[i] contains a list of skills that the person has.
+
+Consider a sufficient team: a set of people such that for every required skill in req_skills, there is at least one person in the team who has that skill. We can represent these teams by the index of each person.
+
+For example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
+Return any sufficient team of the smallest possible size, represented by the index of each person. You may return the answer in any order.
+
+It is guaranteed an answer exists.
+
+ 
+
+Example 1:
+
+Input: req_skills = ["java","nodejs","reactjs"], people = [["java"],["nodejs"],["nodejs","reactjs"]]
+Output: [0,2]
+Example 2:
+
+Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
+Output: [1,2]
+ 
+
+Constraints:
+
+1 <= req_skills.length <= 16
+1 <= req_skills[i].length <= 16
+req_skills[i] consists of lowercase English letters.
+All the strings of req_skills are unique.
+1 <= people.length <= 60
+0 <= people[i].length <= 16
+1 <= people[i][j].length <= 16
+people[i][j] consists of lowercase English letters.
+All the strings of people[i] are unique.
+Every skill in people[i] is a skill in req_skills.
+It is guaranteed a sufficient team exists.
+'''
+
+'''
+Approach :
+
+
+'''
+
+class Solution:
+    def smallestSufficientTeam(self, req_skills: List[str],
+                               people: List[List[str]]) -> List[int]:
+        n = len(people)
+        m = len(req_skills)
+        skill_id = dict()
+        for i, skill in enumerate(req_skills):
+            skill_id[skill] = i
+        skills_mask_of_person = [0] * n
+        for i in range(n):
+            for skill in people[i]:
+                skills_mask_of_person[i] |= 1 << skill_id[skill]
+        dp = [(1 << n) - 1] * (1 << m)
+        dp[0] = 0
+        for skills_mask in range(1, 1 << m):
+            for i in range(n):
+                smaller_skills_mask = skills_mask & ~skills_mask_of_person[i]
+                if smaller_skills_mask != skills_mask:
+                    people_mask = dp[smaller_skills_mask] | (1 << i)
+                    if people_mask.bit_count() < dp[skills_mask].bit_count():
+                        dp[skills_mask] = people_mask
+
+        answer_mask = dp[(1 << m) - 1]
+        ans = []
+        for i in range(n):
+            if (answer_mask >> i) & 1:
+                ans.append(i)
+        return ans
