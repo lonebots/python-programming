c0fa8867ec06b26feb4b21d3bd361e8fc0edbda3
lonebots <jishnusurajila@gmail.com>
2023-07-23 18:38:10 +0530

    lc: 894. all possible full binary tree solution added

---
+++ leet-code/894-all-possible-full-binary-tree.py
@@ -0,0 +1,35 @@
+'''
+Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
+Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
+A full binary tree is a binary tree where each node has exactly 0 or 2 children.
+
+Example 1:
+
+Input: n = 7
+Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if n % 2 == 0 :
+            return []
+        if n == 1 :
+            return [TreeNode()]
+        
+        res = []
+
+        for i in range(1,n,2):
+            left = self.allPossibleFBT(i)
+            right = self.allPossibleFBT(n-i-1)
+
+            for l in left :
+                for r in right :
+                    root = TreeNode(0,l,r)
+                    res.append(root)
+        return res
