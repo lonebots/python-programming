a0a7784791c313b77ff59abfbb50b9562d6895d0
lonebots <jishnusurajila@gmail.com>
2023-07-25 09:25:10 +0530

    lc: 50. pow(x,n) solution added

---
+++ leet-code/50-pow-x-n.py
@@ -0,0 +1,48 @@
+'''
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-104 <= xn <= 104
+'''
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def binExp(x,n):
+            # base case 
+            if n == 0 :
+                return 1.0 
+
+            # negative power
+            if n < 0 :
+                return 1.0/binExp(x,-1*n)
+            
+            # perform binary exponentiation
+            # odd case 
+            if  n % 2 == 1 :
+                return x * binExp(x*x,(n-1)//2) 
+            
+            # case even
+            else :
+                return binExp(x*x,n//2)
+
+        return binExp(x,n)
