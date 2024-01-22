96662125f4b198f60e1d4bc24c34dd1ef01b90d6
lonebots <jishnusurajila@gmail.com>
2024-01-22 23:33:02 +0530

    lc: 645. set missmatch solution added

---
+++ leet-code/645-set-missmatch.py
@@ -0,0 +1,41 @@
+'''
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+
+Example 2:
+
+Input: nums = [1,1]
+Output: [1,2]
+
+ 
+
+Constraints:
+
+    2 <= nums.length <= 104
+    1 <= nums[i] <= 104
+
+'''
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums) 
+        repeat = -1 
+        missing = -1 
+        for i in range(1, n+1) : 
+            count = nums.count(i)
+            if (count == 0) : 
+                missing = i
+            elif (count == 2) : 
+                repeat = i
+        
+        return [repeat, missing]
