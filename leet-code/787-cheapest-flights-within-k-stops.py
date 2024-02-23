e0f3699e13a19388fc3fb889307e20614a15d8b9
lonebots <jishnusurajila@gmail.com>
2024-02-24 00:09:11 +0530

    lc: 787. cheapest flights within k stops solution added

---
+++ leet-code/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,80 @@
+'''
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+
+ 
+
+Example 1:
+
+Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+Output: 700
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
+
+Example 2:
+
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+
+Example 3:
+
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+Output: 500
+Explanation:
+The graph is shown above.
+The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+
+ 
+
+Constraints:
+
+    1 <= n <= 100
+    0 <= flights.length <= (n * (n - 1) / 2)
+    flights[i].length == 3
+    0 <= fromi, toi < n
+    fromi != toi
+    1 <= pricei <= 104
+    There will not be any multiple flights between two cities.
+    0 <= src, dst, k < n
+    src != dst
+
+'''
+
+from collections import defaultdict
+from queue import Queue
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        for flight in flights:
+            adj[flight[0]].append((flight[1], flight[2]))
+
+        dist = [float('inf')] * n
+        dist[src] = 0
+
+        q = Queue()
+        q.put((src, 0))
+        stops = 0
+
+        while not q.empty() and stops <= k:
+            sz = q.qsize()
+            for _ in range(sz):
+                node, distance = q.get()
+
+                if node not in adj: continue
+
+                for neighbour, price in adj[node]:
+                    if price + distance >= dist[neighbour]: continue
+                    dist[neighbour] = price + distance
+                    q.put((neighbour, dist[neighbour]))
+
+            stops += 1
+
+        return dist[dst] if dist[dst] != float('inf') else -1
+
