facb321371e01e22dfc6b2337a3fec7298dbf2c1
lonebots <jishnusurajila@gmail.com>
2024-02-24 14:38:11 +0530

    lc: 2092. find all people with secret solution added

---
+++ leet-code/2092-find-all-people-with-secret.py
@@ -0,0 +1,105 @@
+'''
+Person 0 has a secret and initially shares the secret with a person firstPerson at time 0. This secret is then shared every time a meeting takes place with a person that has the secret. More formally, for every meeting, if a person xi has the secret at timei, then they will share the secret with person yi, and vice versa.
+
+The secrets are shared instantaneously. That is, a person may receive the secret and share it with people in other meetings within the same time frame.
+
+Return a list of all the people that have the secret after all the meetings have taken place. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: n = 6, meetings = [[1,2,5],[2,3,8],[1,5,10]], firstPerson = 1
+Output: [0,1,2,3,5]
+Explanation:
+At time 0, person 0 shares the secret with person 1.
+At time 5, person 1 shares the secret with person 2.
+At time 8, person 2 shares the secret with person 3.
+At time 10, person 1 shares the secret with person 5.​​​​
+Thus, people 0, 1, 2, 3, and 5 know the secret after all the meetings.
+
+Example 2:
+
+Input: n = 4, meetings = [[3,1,3],[1,2,2],[0,3,3]], firstPerson = 3
+Output: [0,1,3]
+Explanation:
+At time 0, person 0 shares the secret with person 3.
+At time 2, neither person 1 nor person 2 know the secret.
+At time 3, person 3 shares the secret with person 0 and person 1.
+Thus, people 0, 1, and 3 know the secret after all the meetings.
+
+Example 3:
+
+Input: n = 5, meetings = [[3,4,2],[1,2,1],[2,3,1]], firstPerson = 1
+Output: [0,1,2,3,4]
+Explanation:
+At time 0, person 0 shares the secret with person 1.
+At time 1, person 1 shares the secret with person 2, and person 2 shares the secret with person 3.
+Note that person 2 can share the secret at the same time as receiving it.
+At time 2, person 3 shares the secret with person 4.
+Thus, people 0, 1, 2, 3, and 4 know the secret after all the meetings.
+
+ 
+
+Constraints:
+
+    2 <= n <= 105
+    1 <= meetings.length <= 105
+    meetings[i].length == 3
+    0 <= xi, yi <= n - 1
+    xi != yi
+    1 <= timei <= 105
+    1 <= firstPerson <= n - 1
+'''
+
+# wrong logic : 
+
+# class Solution:
+#     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+#         personWithSecret = set([0,firstPerson])
+        
+#         meetings.sort(key = lambda x : x[2])
+#         previousTime = 0 
+
+#         for x,y,time in meetings :  
+#             if x in personWithSecret : 
+#                 personWithSecret.add(y)
+#             elif y in personWithSecret :
+#                 personWithSecret.add(x) 
+
+#         return personWithSecret
+
+
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        # For every person, store the time and label of the person met.
+        graph = defaultdict(list)
+        for x, y, t in meetings:
+            graph[x].append((t, y))
+            graph[y].append((t, x))
+        
+        # Earliest time at which a person learned the secret 
+        # as per current state of knowledge. If due to some new information, 
+        # the earliest time of knowing the secret changes, we will update it
+        # and again process all the people whom he/she meets after the time
+        # at which he/she learned the secret.
+        earliest = [inf] * n
+        earliest[0] = 0
+        earliest[firstPerson] = 0
+
+        # Queue for BFS. It will store (person, time of knowing the secret)
+        q = deque()
+        q.append((0, 0))
+        q.append((firstPerson, 0))
+
+        # Do BFS
+        while q:
+            person, time = q.popleft()
+            for t, next_person in graph[person]:
+                if t >= time and earliest[next_person] > t:
+                    earliest[next_person] = t
+                    q.append((next_person, t))
+        
+        # Since we visited only those people who know the secret,
+        # we need to return indices of all visited people.
+        return [i for i in range(n) if earliest[i] != inf]
