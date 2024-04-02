f8ab201871301c6313db8c1180bdb8f0fe28a9ae
lonebots <jishnusurajila@gmail.com>
2024-04-02 22:43:41 +0530

    lc: 205. isomorphic string solution added

---
+++ leet-code/205-isomorphic-string.py
@@ -0,0 +1,38 @@
+'''
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+
+'''
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map_s_t = {}
+        map_t_s = {}
+
+        for c1, c2 in zip(s,t) :
+            if c1 not in map_s_t and c2 not in map_t_s : 
+                map_s_t[c1] = c2 
+                map_t_s[c2] = c1
+            elif map_s_t.get(c1) != c2 or map_t_s.get(c2) != c1 :
+                return False 
+        return True
