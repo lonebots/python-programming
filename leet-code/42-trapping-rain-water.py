365a4273193e81018a593368096f02c61c4edd57
lonebots <jishnusurajila@gmail.com>
2024-04-12 22:29:39 +0530

    lc: 42 trapping rain-water question updated

--- leet-code/42-trapping-rain-water.py
+++ leet-code/42-trapping-rain-water.py
@@ -1,3 +1,23 @@
+''''
+question : 
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+''''
+
+
 '''
 1. here left and right pointers are used to solve 
 2. maxL and maxR points to the maximum neighbouring heights of the current block under consideration (based on left or right you consider)
