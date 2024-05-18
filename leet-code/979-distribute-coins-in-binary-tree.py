fc89b6e07a373e540899fe7afad22fb77c27a1f3
Jishnu ⚡ <jishnusurajila@gmail.com>
2024-05-18 23:15:29 +0530

    lc: 979. distribute-coins-in-binary-tree solution added

---
+++ leet-code/979-distribute-coins-in-binary-tree.py
@@ -0,0 +1,60 @@
+'''
+You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+
+Return the minimum number of moves required to make every node have exactly one coin.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,0,0]
+Output: 2
+Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+Example 2:
+
+
+Input: root = [0,3,0]
+Output: 3
+Explanation: From the left child of the root, we move two coins to the root [taking two moves]. Then, we move one coin from the root of the tree to the right child.
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= n <= 100
+0 <= Node.val <= n
+The sum of all Node.val is n.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        self.moves = 0
+
+        # dfs 
+        def dfs(current) :
+            # base case 
+            if current == None : return 0
+
+            # handle left and right nodes
+            left_coins = dfs(current.left)
+            right_coins = dfs(current.right)
+
+            # add total number of moves to exchanges
+            self.moves+= abs(left_coins) + abs(right_coins)
+
+            # return the number of coins current has available to exchange
+            return (current.val - 1) + left_coins + right_coins
+
+        dfs(root)
+
+        return self.moves
